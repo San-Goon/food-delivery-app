28d0e2402d805308f6e77ae4776c5cf6d2f270b8
San-Goon <tigerlove95@naver.com>
2022-11-26 15:52:42 +0900

    feat: implement scroll in order by using flat list

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,12 +1,75 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import {OrderReturnType} from '../../types';
 
 function Orders() {
+  const orders = useSelector((state: RootState) => state.order.orders);
+
+  const toggleDetail = useCallback(() => {}, []);
+
+  const renderItem = useCallback(
+    ({item}: {item: OrderReturnType}) => {
+      return (
+        <View style={styles.orderContainer}>
+          <Pressable onPress={toggleDetail} style={styles.info}>
+            <Text style={styles.eachInfo}>
+              {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+            </Text>
+          </Pressable>
+        </View>
+      );
+    },
+    [toggleDetail],
+  );
+
   return (
-    <View>
-      <Text>Orders</Text>
-    </View>
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.orderId}
+      renderItem={renderItem}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  orderContainer: {
+    borderRadius: 5,
+    margin: 5,
+    padding: 10,
+    backgroundColor: 'lightgray',
+  },
+  info: {
+    flexDirection: 'row',
+  },
+  eachInfo: {
+    flex: 1,
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+  },
+  acceptButton: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
+    flex: 1,
+  },
+  rejectButton: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomRightRadius: 5,
+    borderTopRightRadius: 5,
+    flex: 1,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
 
 export default Orders;
